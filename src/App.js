38f8f27f0d529a446620ca0968e6019b7a57c730
ndragun <39500267+ndragun92@users.noreply.github.com>
2022-04-05 19:25:01 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+const { useState } = React;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // Example with passing custom data in slots & props
+    const [value, setValue] = useState('');
+    const handleChange = (e) => setValue(e.target.value);
+    React.useEffect(() => {
+        const element = document.getElementById('wc-qrcg-content')
+        if(element) {
+            element.addEventListener('submit', () => {alert('Submitted');})
+        }
+    });
+      return (
+        <div className="App">
+            <wc-qrcg-navbar>
+                <ul>
+                    <li>
+                        <a href="#mytest">My test</a>
+                    </li>
+                </ul>
+            </wc-qrcg-navbar>
+            <wc-qrcg-content id='wc-qrcg-content' data-content={value} />
+            <hr/>
+            <input type="text" value={value} onChange={handleChange} />
+        </div>
+      );
+    // Example when including whole layout
+    // React.useEffect(() => {
+    //     const element = document.getElementById('wc-qrcg-app')
+    //     if(element) {
+    //         element.addEventListener('submit', () => {alert('Submitted');})
+    //     }
+    // });
+    // return (
+    //     <div className="App">
+    //         <wc-qrcg-app id='wc-qrcg-app' data-content='My static data content from react'>
+    //             <div slot='content'>
+    //                 Hello from React.js
+    //             </div>
+    //         </wc-qrcg-app>
+    //     </div>
+    // );
 }
 
 export default App;
